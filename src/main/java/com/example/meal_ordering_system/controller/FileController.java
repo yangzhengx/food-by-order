60c35320606a87db613528f7a3a8e228d8a69234
lww <2015497388@qq.com>
2022-05-19 15:07:49 +0800

    文件上传模块 -------liweiwei

---
+++ src/main/java/com/example/meal_ordering_system/controller/FileController.java
@@ -0,0 +1,9 @@
+package com.example.meal_ordering_system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("upload")
+public class FileController {
+}
